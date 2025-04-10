4324f2706edf56c2a2e7df2ca2cbd19622d6ee8c
Oren Leavitt <oleavitt@ix.netcom.com>
2025-04-09 19:48:53 -0500

    Add skeleton starter project and generated RickAndMorty API response model

---
+++ RickyAndMortyApp/RickyAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyAndMortyAppApp.swift
+//  RickyAndMortyApp
+//
+//  Created by Oren Leavitt on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct RickyAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
